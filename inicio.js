d1a206cc1754954e0657a35b86bf017e6f62f12e
Araceli-gp <araceliguzman0620@gmail.com>
2023-05-05 00:39:49 -0500

    pollito

--- inicio.js
+++ inicio.js
@@ -1,7 +1,34 @@
-function a_form(){
+//FORMULARIO
+function a_form() {
     document.getElementById('uno').style.display = 'flex';
-    document.getElementById('cero').style.display ='none';
+    document.getElementById('cero').style.display = 'none';
 }
+
+function s_form() {
+    document.getElementById('dos').style.display = 'flex';
+    document.getElementById('uno').style.display = 'none';
+    document.getElementById('cero').style.display = 'none';
+}
+
+function e_form() {
+    document.getElementById('dos').style.display = 'none';
+    document.getElementById('uno').style.display = 'none';
+    document.getElementById('cero').style.display = 'none';
+    document.getElementById('tres').style.display = 'flex';
+
+}
+
+
 $("#comenzar").click(function (e) {
     e.preventDefault();
 });
+
+$("#btn_dos").click(function (e) {
+    e.preventDefault();
+});
+
+$("#btn_tres").click(function (e) {
+    e.preventDefault();
+});
+
+//FIN FE FORMULARIO
