e17509f7ae9d9b3c328731f813fa067b939f8952
Araceli-gp <araceliguzman0620@gmail.com>
2023-05-04 23:46:04 -0500

    pollito

---
+++ inicio.js
@@ -0,0 +1,7 @@
+function a_form(){
+    document.getElementById('uno').style.display = 'flex';
+    document.getElementById('cero').style.display ='none';
+}
+$("#comenzar").click(function (e) {
+    e.preventDefault();
+});
